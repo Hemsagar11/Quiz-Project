592334c309e19e6feb6a7760e398004aa022f757
whitedevil <hsrreddy11@protonmail.com>
2024-11-09 09:47:29 +0530

    Initial commit

---
+++ app/src/main/java/com/hemsagar/masterquiz/FlashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.hemsagar.masterquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FlashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
